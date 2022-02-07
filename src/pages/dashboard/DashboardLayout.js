fd44ad8e75b7ed1a8d7d093bd47ed1a03adbe36a
Nikita Lavrenko <n_lavrenko@indatalabs.com>
2022-02-07 22:11:07 +0300

    Sign Out is working.

--- src/pages/dashboard/DashboardLayout.js
+++ src/pages/dashboard/DashboardLayout.js
@@ -1,174 +1,174 @@
-import * as React from 'react';
-import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import MuiDrawer from '@mui/material/Drawer';
-import Box from '@mui/material/Box';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import Badge from '@mui/material/Badge';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import { Outlet } from 'react-router-dom'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import LogoutIcon from '@mui/icons-material/Logout'
+import MenuIcon from '@mui/icons-material/Menu'
+import MuiAppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import CssBaseline from '@mui/material/CssBaseline'
+import Divider from '@mui/material/Divider'
+import MuiDrawer from '@mui/material/Drawer'
+import Grid from '@mui/material/Grid'
+import IconButton from '@mui/material/IconButton'
+import List from '@mui/material/List'
+import Paper from '@mui/material/Paper'
+import {createTheme, styled, ThemeProvider} from '@mui/material/styles'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import * as React from 'react'
+import {Outlet} from 'react-router-dom'
 import {Copyright} from '../../components/Copyright'
-import { mainListItems, secondaryListItems } from './listItems';
+import useAuth from '../../hooks/useAuth'
+import {mainListItems, secondaryListItems} from './listItems'
 
 
-const drawerWidth = 240;
+const drawerWidth = 240
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
+  shouldForwardProp: (prop) => prop !== 'open'
+})(({theme, open}) => ({
   zIndex: theme.zIndex.drawer + 1,
   transition: theme.transitions.create(['width', 'margin'], {
     easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
+    duration: theme.transitions.duration.leavingScreen
   }),
   ...(open && {
     marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
+    width: `calc(100% - ${ drawerWidth }px)`,
     transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
+      duration: theme.transitions.duration.enteringScreen
+    })
+  })
+}))
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
+const Drawer = styled(MuiDrawer, {shouldForwardProp: (prop) => prop !== 'open'})(
+  ({theme, open}) => ({
     '& .MuiDrawer-paper': {
       position: 'relative',
       whiteSpace: 'nowrap',
       width: drawerWidth,
       transition: theme.transitions.create('width', {
         easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
+        duration: theme.transitions.duration.enteringScreen
       }),
       boxSizing: 'border-box',
       ...(!open && {
         overflowX: 'hidden',
         transition: theme.transitions.create('width', {
           easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.leavingScreen,
+          duration: theme.transitions.duration.leavingScreen
         }),
         width: theme.spacing(7),
         [theme.breakpoints.up('sm')]: {
-          width: theme.spacing(9),
-        },
-      }),
-    },
-  }),
-);
+          width: theme.spacing(9)
+        }
+      })
+    }
+  })
+)
 
-const mdTheme = createTheme();
+const mdTheme = createTheme()
 
 function DashboardContent() {
-  const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = React.useState(true)
   const toggleDrawer = () => {
-    setOpen(!open);
-  };
+    setOpen(!open)
+  }
+  const {signOut} = useAuth()
   
   return (
-    <ThemeProvider theme={mdTheme}>
-      <Box sx={{ display: 'flex' }}>
+    <ThemeProvider theme={ mdTheme }>
+      <Box sx={ {display: 'flex'} }>
         <CssBaseline />
-        <AppBar position="absolute" open={open}>
+        <AppBar position='absolute' open={ open }>
           <Toolbar
-            sx={{
-              pr: '24px', // keep right padding when drawer closed
-            }}
+            sx={ {
+              pr: '24px' // keep right padding when drawer closed
+            } }
           >
             <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={toggleDrawer}
-              sx={{
+              edge='start'
+              color='inherit'
+              aria-label='open drawer'
+              onClick={ toggleDrawer }
+              sx={ {
                 marginRight: '36px',
-                ...(open && { display: 'none' }),
-              }}
+                ...(open && {display: 'none'})
+              } }
             >
               <MenuIcon />
             </IconButton>
             <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
+              component='h1'
+              variant='h6'
+              color='inherit'
               noWrap
-              sx={{ flexGrow: 1 }}
+              sx={ {flexGrow: 1} }
             >
               Dashboard
             </Typography>
-            <IconButton color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <NotificationsIcon />
-              </Badge>
+            <IconButton color='inherit' onClick={ signOut }>
+              Sign Out &nbsp;{ ' ' }
+              <LogoutIcon />
             </IconButton>
           </Toolbar>
         </AppBar>
-        <Drawer variant="permanent" open={open}>
+        <Drawer variant='permanent' open={ open }>
           <Toolbar
-            sx={{
+            sx={ {
               display: 'flex',
               alignItems: 'center',
               justifyContent: 'flex-end',
-              px: [1],
-            }}
+              px: [1]
+            } }
           >
-            <IconButton onClick={toggleDrawer}>
+            <IconButton onClick={ toggleDrawer }>
               <ChevronLeftIcon />
             </IconButton>
           </Toolbar>
           <Divider />
-          <List component="nav">
-            {mainListItems}
-            <Divider sx={{ my: 1 }} />
-            {secondaryListItems}
+          <List component='nav'>
+            { mainListItems }
+            <Divider sx={ {my: 1} } />
+            { secondaryListItems }
           </List>
         </Drawer>
         <Box
-          component="main"
-          sx={{
+          component='main'
+          sx={ {
             backgroundColor: (theme) =>
               theme.palette.mode === 'light'
                 ? theme.palette.grey[100]
                 : theme.palette.grey[900],
             flexGrow: 1,
             height: '100vh',
-            overflow: 'auto',
-          }}
+            overflow: 'auto'
+          } }
         >
           <Toolbar />
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
+          <Container maxWidth='lg' sx={ {mt: 4, mb: 4} }>
+            <Grid container spacing={ 3 }>
+              <Grid item xs={ 12 }>
                 <Paper
-                  sx={{
+                  sx={ {
                     p: 2,
                     display: 'flex',
                     flexDirection: 'column',
-                    height: 240,
-                  }}
+                    height: 240
+                  } }
                 >
                   <Outlet />
                 </Paper>
               </Grid>
             </Grid>
-            <Copyright sx={{ pt: 4 }} />
+            <Copyright sx={ {pt: 4} } />
           </Container>
         </Box>
       </Box>
     </ThemeProvider>
-  );
+  )
 }
 
 export default function DashboardLayout() {
-  return <DashboardContent />;
+  return <DashboardContent />
 }
