a70744b978ece8c6eb13180ea0463a9699c43e15
Nikita Lavrenko <n_lavrenko@indatalabs.com>
2022-02-07 22:30:25 +0300

    Setup baseUrl for Heroku

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const isProd = window.location.origin.includes('heroku')
 let axiosInstance = axios.create({
-  baseURL: isProd ? window.location.origin : 'http://localhost:1080'
+  baseURL: isProd ? 'https://exodus-backend.herokuapp.com' : 'http://localhost:1080'
 })
 
 axiosInstance.interceptors.response.use(
