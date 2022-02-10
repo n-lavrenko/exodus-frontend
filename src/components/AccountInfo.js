f97f5a6723c31c1c0685593f68aa457f93c83426
Nikita Lavrenko <n_lavrenko@indatalabs.com>
2022-02-10 11:17:47 +0300

    Beautify Account Info sidebar.

--- src/components/AccountInfo.js
+++ src/components/AccountInfo.js
@@ -1,6 +1,7 @@
 import useAccount from '../hooks/useAccount'
 import useAuth from '../hooks/useAuth'
-
+import CheckCircleOutlinedIcon from '@mui/icons-material/CheckCircleOutlined';
+import WarningAmberOutlinedIcon from '@mui/icons-material/WarningAmberOutlined';
 
 export function AccountInfo() {
   const {user} = useAuth()
@@ -12,7 +13,11 @@ export function AccountInfo() {
       <br/>
       Email: { user.email }
       <br/>
-      Linked bank account: {isPlaidLinked ? 'YES' : 'NO'}
+      
+      Linked bank account: {isPlaidLinked ?
+      <div className={'link-account-status yes'}><CheckCircleOutlinedIcon/></div> :
+      <div className={'link-account-status no'}><WarningAmberOutlinedIcon/></div>
+    }
     </div>
   )
 }
